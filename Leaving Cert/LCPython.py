bb7c56b751c0a9c50588b5bae714b7d15e71c9d3
alexcdee <87585628+alexcdee@users.noreply.github.com>
2024-02-26 10:27:38 +0000

    Update LCPython.py

--- Leaving Cert/LCPython.py
+++ Leaving Cert/LCPython.py
@@ -1,16 +1,40 @@
 from pandas import *
-dataIn = read_csv("data.csv")
-
-steps = dataIn['steps'].tolist()
-print(steps)
-light = dataIn['light'].tolist()
-print(light)
-time = dataIn['time'].tolist()
-print(time)
-
 import statistics
 import matplotlib.pyplot as plt
 
+dataIn = read_csv("data.csv")
+
+steps = dataIn['steps'].tolist()
+print('Steps',steps)
+light = dataIn['light'].tolist()
+print('Lights',light)
+time = dataIn['time'].tolist()
+print('Time',time)
+print('The mean is of steps:',round(statistics.mean(steps)))
+print('The mode is of steps:',statistics.mode(steps))
+print('The median is fo steps:',round(statistics.median(steps)))
+
+'''
+plt.title("Step Tracker Data", loc = 'left')
 plt.plot(steps,light,time)
 plt.show()
+'''
+
+# What if Questions
+def switch(user):
+    if user == 1:
+        one = input('What did you do before?' )
+        return 'ok'
+    elif user == 2:
+        return '2'
+    elif user == 3:
+        return '3'
+    elif user == 4:
+        return '4'
+    elif user == 5:
+        return '5'
+
+
+user = int(input('On a scale through 1-5. How energized do you feel after the walk/jog/run? '))
+print(switch(user))
 
